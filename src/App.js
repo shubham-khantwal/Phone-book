47cbe71b6c3120e52a588acb7c45ea8e6ef1439e
Shubham Khantwal <skhantwal84@gmail.com>
2021-02-19 21:12:14 +0530

    removed unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {useState} from "react";
 function App() {
+
+  const [name,setName] = useState("");
+  const [ph,setPh] = useState("");
+  const [email,setEmail] = useState("");  
+
+  const submitMessage = ()=>{
+    console.log("Details fetched !");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="Information">
+          <label>Name : </label>
+          <input type="text" onChange = {(event)=>{
+            setName(event.target.value);
+          }
+          }
+          /><br/>
+          <label>Ph. No. : </label>
+          <input type="text" onChange = {(event)=>{
+            setPh(event.target.value);
+          }
+          }
+          /><br/>
+          <label>Email : </label>
+          <input type="text" onChange = {(event)=>{
+            setEmail(event.target.value);
+          }
+          }
+          /><br/>
+          <button onClick={submitMessage}>Add Phone Details</button>
+        </div>
     </div>
   );
 }
